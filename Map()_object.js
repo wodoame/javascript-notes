db8bc13b32ae0bb31e16ffef0ac84f585e94bcd6
Bernard Mawulorm Kofi Wodoame <98283377+wodoame@users.noreply.github.com>
2023-02-16 08:51:35 +0000

    Update Map()_object.js

--- Map()_object.js
+++ Map()_object.js
@@ -26,3 +26,13 @@ console.log(map); // Map {"key1" => "value1", "key3" => "value3"}
 
 console.log(map.get("key2")); // undefined
 console.log(map.get("key3")); // "value3"
+
+/*
+The time complexity of obtaining an item from a Map or HashMap is O(1) on average. 
+This means that the time it takes to access an item in a Map or HashMap does not increase with the size of the Map or HashMap.
+
+In the worst case, however, the time complexity of obtaining an item from a Map or HashMap is O(n), where n is the number of elements in the Map or HashMap.
+This can occur when there are hash collisions or when the hash function used by the Map or HashMap is poorly designed, causing a large number of collisions. 
+However, these situations are rare in practice, and the amortized time complexity for obtaining an item from a Map or HashMap is still O(1).
+
+*/
