134ef77abfb3566f61b12142a4f5bce70ce07cb2
Bernard Mawulorm Kofi Wodoame <98283377+wodoame@users.noreply.github.com>
2023-02-17 11:57:24 +0000

    Update .reduce()_method.js

--- .reduce()_method.js
+++ .reduce()_method.js
@@ -11,7 +11,7 @@
 
 // Example:
 const numbers = [1, 2, 3, 4];
-const sum = numbers.reduce((total, num) => total + num, 0);
+const sum = numbers.reduce((total, num) => {return total + num;} , 0);
 console.log(sum); // Output: 10
 
 // The reducer function takes two arguments: the accumulator and the current value. 
